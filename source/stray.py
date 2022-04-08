5b394376a9c855241456e03b85d504a86b78c326
Lemmanuel1812 <lotimanu1812@gmail.com>
2022-04-08 08:23:34 +0200

    Final version of stray

--- source/stray.py
+++ source/stray.py
@@ -8,7 +8,15 @@ class Stray (monster.Monster) :
     def __init__(self, anim, times, coord, hp, player) :
         super().__init__(anim, times, coord, hp)
         self.player = player
-        
-    def stray_move (self) :
-        self.coord = algo.a_star_path(self.map, self.coord, self.player)[0]
-    
+     
+    def pulse (self, map:list, played:bool) :
+        """
+        A refresh function called each frame
+        you can do what you want here
+
+        input :
+            - map : a double array wich represent a map of the level (see Level class)
+            - played : if we have to resolve the player action
+        """
+        if played == True :
+            self.coord = algo.a_star_path(self.map, self.coord, self.player)[0]
